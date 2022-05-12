6c3d402ffbafc2caac0810d925904b2ec5d8f44e
Iain Stewart <iainstewart08@gmail.com>
2022-05-12 22:29:17 +0100

    Display ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,22 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const { expenseDate, expenseTitle, expenseAmount } = props;
+
+  const month = expenseDate.toLocaleString('en-US', { month: 'long' });
+  const year = expenseDate.getFullYear();
+  const day = expenseDate.toLocaleString('en-US', { day: '2-digit' });
+
   return (
     <div className='expense-item'>
-      <div>March 28th 2021</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>{expenseAmount}</div>
       </div>
     </div>
   );
