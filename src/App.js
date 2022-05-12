cbece5763eba79c36f9eaee43e9ccf1a25468334
Iain Stewart <iainstewart08@gmail.com>
2022-05-13 00:08:45 +0100

    Extract Expenses and ExpenseDate component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -26,26 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        expenseDate={expenses[0].date}
-        expenseTitle={expenses[0].title}
-        expenseAmount={expenses[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        expenseDate={expenses[1].date}
-        expenseTitle={expenses[1].title}
-        expenseAmount={expenses[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        expenseDate={expenses[2].date}
-        expenseTitle={expenses[2].title}
-        expenseAmount={expenses[2].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        expenseDate={expenses[3].date}
-        expenseTitle={expenses[3].title}
-        expenseAmount={expenses[3].amount}
-      ></ExpenseItem>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,12 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
   const { expenseDate, expenseTitle, expenseAmount } = props;
 
-  const month = expenseDate.toLocaleString('en-US', { month: 'long' });
-  const year = expenseDate.getFullYear();
-  const day = expenseDate.toLocaleString('en-US', { day: '2-digit' });
-
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={expenseDate} />
       <div className='expense-item__description'>
         <h2>{expenseTitle}</h2>
         <div className='expense-item__price'>{expenseAmount}</div>
