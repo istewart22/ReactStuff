ba97913ae7e01389385d1721a571960f734aa2ef
Iain Stewart <iainstewart08@gmail.com>
2022-05-15 00:44:17 +0100

    Add button to update title using setState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,25 @@
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
   const { expenseDate, expenseTitle, expenseAmount } = props;
+
+  const [title, setTitle] = useState(expenseTitle);
+
+  const clickHandler = () => {
+    setTitle('Updated!!');
+  };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={expenseDate} />
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>{expenseAmount}</div>
       </div>
+      <button onClick={clickHandler}>Update title</button>
     </Card>
   );
 };
