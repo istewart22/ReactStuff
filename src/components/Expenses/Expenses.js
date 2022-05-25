7f45739fbbfb4f182a9147ca4ec4aec71554932d
Iain Stewart <iainstewart08@gmail.com>
2022-05-25 08:11:56 +0100

    Refactor to dynamically render expense list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,32 +11,20 @@ const Expenses = (props) => {
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log(selectedYear);
   };
 
   return (
     <Card className='expenses'>
       <ExpensesFilter onChangeFilter={filterChangeHandler} />
-      <ExpenseItem
-        expenseDate={expenses[0].date}
-        expenseTitle={expenses[0].title}
-        expenseAmount={expenses[0].amount}
-      />
-      <ExpenseItem
-        expenseDate={expenses[1].date}
-        expenseTitle={expenses[1].title}
-        expenseAmount={expenses[1].amount}
-      />
-      <ExpenseItem
-        expenseDate={expenses[2].date}
-        expenseTitle={expenses[2].title}
-        expenseAmount={expenses[2].amount}
-      />
-      <ExpenseItem
-        expenseDate={expenses[3].date}
-        expenseTitle={expenses[3].title}
-        expenseAmount={expenses[3].amount}
-      />
+
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          expenseAmount={expense.amount}
+          expenseDate={expense.date}
+          expenseTitle={expense.title}
+        />
+      ))}
     </Card>
   );
 };
